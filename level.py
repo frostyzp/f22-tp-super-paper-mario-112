09c66d4360db7a34e9dd5f0b900d9bbaca7dc73a
frostyzp <91027903+frostyzp@users.noreply.github.com>
2022-11-27 11:04:32 -0500

    11/27 - Pre terrain generation

--- level.py
+++ level.py
@@ -2,48 +2,45 @@ import random
 from cmu_112_graphics import *
 import math
 
-
 ##########################################
-# 
+# Terrain generator across 2d lists
 ##########################################
 
 class genLevel(object):
+    rows = 30
+    cols = 240
+    rawTerrain = [(240 * [0]) for r in range(rows)]
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, platforms):
+        self.platforms = platforms
 
     # With reference from https://arpit.substack.com/p/1d-procedural-terrain-generation
     def terrainArray(self, app):
-
         # Once xscroll hits certain intervals, draw a chunk of terrain
         # a gap will be left in between each piece of terrain
 
-        # Linear interpolation
-        # terrain = [(random.randrange(app.width),
-        #           random.randrange(60, app.height)) for _ in range(50)]
         rows, cols = 30, 240
         terrain = [(cols * [0]) for r in range(rows)]
 
         for col in range(1, 16):
-            terrain[25][col] = 'tile'
+            terrain[25][col] = ''
 
         for col in range(25, 30):
-            terrain[28][col] = 'tile'
-        #2D list - X, Y, Width
-        # terrain = [[1,10,0],[2, 10,0]] #[200,500,200], [800, 300, 200]]
+            terrain[28][col] = ''
         return terrain
 
-    # Estimate points between known points
-    def cosineInterp(a, b, mu):
-        mu2 = (1 - math.pi(mu * math.pi)) / 2
-        return a * (1 - mu2) + b * mu2
 
-    # Estimate intermediate points 
-    def linearInterp(a, b, mu):
-        return a * (1 - mu) + b * mu 
+    def platFormsArray(self, terrain):
+        # Will generate on only one y axis
+        for i in range(0, self.platform):
+            # random x values, but ensure there are no overlaps
+            x = 50
+            # generate platforms of length 5
+            for col in range(x, x+5):
+                terrain[col][20] = ''
 
+        return terrain
 
-    def platFormsArray(self, app):
-        plaforms = 0
+ # With reference from https://bitesofcode.wordpress.com/2016/12/23/landscape-generation-using-midpoint-displacement/
 
-        return 
+ 
